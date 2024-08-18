9432abd896fc18fff4d9f85fd768a324bcdd3748
pengyunfei <cloudflypeng@gmail.com>
2024-08-18 09:11:22 +0800

    chore: 尝试构建定义文件

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,18 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/main.ts'),
       name: 'WebextFetch',
       fileName: (format) => `webext-fetch.${format}.js`,
-    }
-  }
+    },
+    rollupOptions: {
+      external: ['chrome'],
+      output: {
+        globals: {
+          chrome: 'chrome',
+        },
+        // 添加以下配置以生成声明文件
+        sourcemap: true,
+      },
+    },
+    // 添加以下配置以生成声明文件
+    // declaration: true,
+  },
 })
