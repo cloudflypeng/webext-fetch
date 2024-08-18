0f6f10f96d85dc8c38cb0019bf85babf3f6f434b
pengyunfei <cloudflypeng@gmail.com>
2024-08-18 09:27:36 +0800

    chore: 生成类型文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,23 @@
 // 库模式打包src/main.ts
 import { defineConfig } from 'vite'
 import path from 'path'
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [
+    dts({
+      outputDir: 'dist/types',  // 声明文件的输出目录
+      include: ['src/**/*.ts'], // 包含的文件
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
       name: 'WebextFetch',
       fileName: (format) => `webext-fetch.${format}.js`,
     },
-    rollupOptions: {
-      external: ['chrome'],
-      output: {
-        globals: {
-          chrome: 'chrome',
-        },
-        // 添加以下配置以生成声明文件
-        sourcemap: true,
-      },
-    },
-    // 添加以下配置以生成声明文件
-    // declaration: true,
+
+
+
   },
 })
